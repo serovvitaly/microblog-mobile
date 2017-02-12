40f7812f66a41216907d0805f57ae7a471f4a448
root <root@ih241223.vds.myihor.ru>
2017-02-12 12:45:57 -0500

    issue #7

--- blog/views.py
+++ blog/views.py
@@ -23,7 +23,7 @@ class IndexView(generic.TemplateView):
         else:
             posts = Post.objects.filter(is_active__exact=True).all()
         return {
-            'items': posts[0:10],
+            'items': posts,
             'wrapper_widget': 'widget/multi-column.html',
             'item_widget': 'widget/post-mini.html',
             'is_editor': is_editor,
@@ -88,7 +88,7 @@ class PostView(generic.TemplateView):
             'RESULT_PAGE': '/hello/',
             'item': self.post,
             'post_content': markdown.markdown(post_content),
-            'posts': Post.objects.filter(is_active__exact=True)[0:10],
+            'posts': Post.objects.filter(is_active__exact=True),
             'is_editor': self.request.user.has_perm('blog.change_post'),
             'sub_posts': Post.objects.all()[0:6],
         }
